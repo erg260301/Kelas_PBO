edee63c8cbd9fe82d818031cfb51e2a8eadc46e6
unknown <jambore27@gmail.com>
2021-02-26 02:47:50 +0700

    done

---
+++ aktifitas3.py
@@ -0,0 +1,13 @@
+print("percabangan pada bahasa pemrograman Python")
+print("pilih bahasa Pemrograman: ")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2)"))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("bahasa pemrograman yang dipilih adalah C")
+else :
+    print("pilihan anda tidak tersedia!!!3")
